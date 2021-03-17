0361e25a87151d512fd7ed24b30f032a6c86591f
Alexander Zotz <AlexMitDemBart@gmail.com>
2021-03-17 20:25:51 +0100

    first commit

---
+++ readme.md
@@ -0,0 +1,7 @@
+# Description
+
+Movie app which showing the currently most popular movies
+
+## Image
+
+![image info](./Images/movies.png)
---
+++ script.js
@@ -0,0 +1,28 @@
+const APIURL = 'https://api.themoviedb.org/3/discover/movie?api_key=04c35731a5ee918f014970082a0088b1&language=en&sort_by=popularity.desc&include_adult=false&include_video=false&page=1'
+const IMAGEURL = 'https://image.tmdb.org/t/p/w500'
+
+getMovies()
+
+async function getMovies(){
+    const response = await fetch(APIURL)
+    const movieData = await response.json();
+
+    movieData.results.forEach((movie) => {
+        const main = document.querySelector('main')
+        const movieEl = document.createElement('div')
+        movieEl.classList.add('movie')
+
+        movieEl.innerHTML = `
+        <img 
+            src=${IMAGEURL + movie.poster_path}
+        />
+        <div class="movie-info">
+            <h3>${movie.title}</h3>
+            <span>${movie.vote_average}</span>
+        </div>       
+        `;
+
+        main.appendChild(movieEl)
+
+    });
+}
